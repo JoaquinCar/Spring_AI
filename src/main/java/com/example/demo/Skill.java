6565f2b88684c3b75f4ba40ba9325256738d8722
JoaquinCar <joaquincarmona758@gmail.com>
2025-07-09 09:36:04 -0600

    repository

--- src/main/java/com/example/demo/Skill.java
+++ src/main/java/com/example/demo/Skill.java
@@ -25,5 +25,29 @@ public class Skill {
         this.name = name;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SoftwareEngineer> getEngineers() {
+        return engineers;
+    }
+
+    public void setEngineers(List<SoftwareEngineer> engineers) {
+        this.engineers = engineers;
+    }
+
     // Otros métodos...
 }
